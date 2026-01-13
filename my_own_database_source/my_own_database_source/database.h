ed3d65cdd32ff14ef0f0c58dbcad92cd5f2bac20
Noah <1476021453@qq.com>
2026-01-13 14:07:06 +0800

    IDE:VS2022 更新数据库 SQL_VM 头文件源代码 在原先 REPL 代码基础上新增 SQL_VM 功能模块

--- my_own_database_source/my_own_database_source/database.h
+++ my_own_database_source/my_own_database_source/database.h
@@ -14,12 +14,51 @@ typedef int ssize_t;
 #include <sys/types.h>
 #endif
 
+// -----------------------------------
+// REPL 使用结构体声明
 // 输入缓冲区类型
 typedef struct {
 	char* buffer;			// 缓冲区
 	size_t buffer_length;	// 缓冲区长度
 	ssize_t input_length;	// 输入长度
 } InputBuffer;
+
+
+// --------------------------------
+// SQL_VM 使用结构体声明
+//@@ - 10, 6 + 10, 23 @@ struct InputBuffer_t {
+//} InputBuffer;
+
+// @@ - 10, 6 + 10, 23 @@ —— 版本控制信息
+// @@ …… @@ —— 差异化头部
+// - —— 原版本
+// 10 —— 第十行
+// 6 —— 改动6行
+// + —— 现版本
+// 10 —— 第十行
+// 23 —— 改动23行
+
+typedef enum {
+    META_COMMAND_SUCCESS,						// meta 命令成功
+    META_COMMAND_UNRECOGNIZED_COMMAND			// meta 命令无法识别
+} MetaCommandResult;							// meta 命令结果
+
+typedef enum { 
+	PREPARE_SUCCESS,							// 准备成功
+	PREPARE_UNRECOGNIZED_STATEMENT				// 准备无法识别状态
+} PrepareResult;								// 准备结果
+
+typedef enum { 
+	STATEMENT_INSERT,							// 插入状态
+	STATEMENT_SELECT							// 选择状态
+} StatementType;								// 状态类型
+
+typedef struct {
+	StatementType type;							// 状态类型变量 
+} Statement;									// 状态
+
+// ----------------------------------
+// REPL 使用函数声明
 
 // 开辟新的输入缓冲区
 InputBuffer* new_input_buffer();
@@ -31,3 +70,7 @@ void read_input(InputBuffer* input_buffer);
 void close_input_buffer(InputBuffer* input_buffer);
 // 测试 读取-求值（执行）-输出循环
 void test_REPL();
+
+// ----------------------------------------
+// SQL_VM 使用函数声明
+
